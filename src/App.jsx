21e1e051c3b8d35fae02c1e2fef4175d23dd7add
vkhinvasara <vaibhavkhinvasara@gmail.com>
2024-10-29 22:32:53 +0530

    minor changes 5

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import ShoppingCartPage from './pages/ShoppingCartPage';
 function App() {
   console.log("I'm in!")
   return (
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductListingPage />} />
         <Route path="/cart" element={<ShoppingCartPage />} />
       </Routes>
+      </Router>
   );
 }
 
